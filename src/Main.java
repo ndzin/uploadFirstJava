e9bb6accf8b06d2e7714871c6aaa468848d289df
Nathan Dias <astfdatamining@gmail.com>
2022-06-07 19:44:26 -0300

    commit on 'main'

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import java.util.Locale;
 
 public class Main {
 
-    int idade = 25;
-    double altura = 1.68;
-    char sexo = 'F';
-
     public static void main(String[] args) {
+
+        int idade = 25;
+        double altura = 1.624218;
+        char sexo = 'F';
+
         System.out.println("Olá Mundo!");
+        System.out.printf("%.2f%n", altura);
+        System.out.printf("%.4f%n", altura);
+        Locale.setDefault(Locale.US);
+        System.out.printf("%.4f%n", altura);
+        System.out.printf("A sua altura é: %.2f metros%n", altura);
+        
     
     }
 }
