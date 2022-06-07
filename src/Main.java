46967b3ced4c16933b1b9f9ec465345295d1c6a8
Nathan Dias <astfdatamining@gmail.com>
2022-06-07 19:01:34 -0300

    commit on 'main'

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
 
+    int idade = 25;
+    double altura = 1.68;
+    char sexo = 'F';
+
     public static void main(String[] args) {
         System.out.println("Olá Mundo!");
     
