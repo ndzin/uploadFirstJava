db1f457568ceb67145dda748b486f9f1021f7ee3
Nathan Dias <astfdatamining@gmail.com>
2022-06-07 23:31:15 -0300

    commit on 'main'

---
+++ src/Exercise2.java
@@ -0,0 +1,19 @@
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+
+        double bmen, bmai, h, area;
+
+        bmen = 6;
+        bmai = 8;
+        h = 5;
+
+        area = (bmen + bmai) / 2 * h;
+
+        System.out.println(area);
+
+    }
+
+    
+}
