16f2b0fd101019b36cacab4f8cc177b3a5872682
Nathan Dias <astfdatamining@gmail.com>
2022-06-07 23:53:22 -0300

    commit on 'main'

---
+++ src/Exercise3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Exercise3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Type the sus password:");
+        
+        Scanner sc = new Scanner(System.in);
+        String password;
+
+        password = sc.next();
+
+        System.out.println("Correct password, you typed "+password);
+
+
+       sc.close();
+
+    }
+
+
+}
